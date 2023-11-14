1af892801ef1dcbe9ad358df503853bc857b5340
Stepan Isayev <140114944+isastep@users.noreply.github.com>
2023-11-14 22:58:15 +0100

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,13 +1,17 @@
-        int[] data = new int[10];
-        for (int item : data) {
-    public static void main(String[] args) {
+main();
+        data.push(Math.floor(Math.random() * 100) + 1);
 
-        }
-            data[i] = (int) (Math.random() * 100) + 1;
-}
-            System.out.println("Random Number: " + item);
-public class RandomDataGenerator {
-
+    const data = generateRandomData();
     }
-        }
-        for (int i = 0; i < 10; i++) {
+    });
+
+}
+}
+    return data;
+function generateRandomData() {
+    data.forEach(item => {
+    const data = [];
+
+        console.log("Random Number: " + item);
+function main() {
+    for (let i = 0; i < 10; i++) {
